8b7650db03f3cb785cd7412cfddc29fd93bb8294
Констандян Размик Борисович <konstandyan@phystech.edu>
2016-03-14 17:11:52 +0300

    Add dockstrings to all view functions

--- minstagram/userpage/views.py
+++ minstagram/userpage/views.py
@@ -1,18 +1,21 @@
 from django.contrib.auth.models import User
-from django.core.urlresolvers import resolve
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import UserPageData, UserProfileData
 from .forms import MakePostForm, EditInfo, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth.forms import (
-    AuthenticationForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm, UserCreationForm
-)
-from django.contrib.auth.forms import UserCreationForm
 
-# create my first view
+
 def go_home(request):
+    """Method shows to a user his posts.
+
+    Args:
+        request: http request from the user.
+    Returns:
+        HttpResponse: http response that shows "userpage/myposts.html".
+    """
+
     if request.user.is_anonymous():
         return render(request, "userpage/index.html")
     posts_list = UserPageData.objects.filter(user=request.user).order_by("-time_created")
@@ -25,10 +28,25 @@ def go_home(request):
 
 
 def regok(request):
+    """Method indicates about successful registration.
+
+    Args:
+        request: http request from a user.
+    Returns:
+        HttpResponse: http response that shows "registration/OK.html".
+    """
     return render(request, "registration/OK.html")
 
 
 def register(request, form=UserRegistrationForm()):
+    """Method allows a new user to sign up.
+
+    Args:
+        request: http request from the user.
+    Returns:
+        HttpResponse: http response that shows "registration/signup.html"
+        if the form is not valid and does redirect to regok else.
+    """
     if request.method == 'POST':
         data = UserRegistrationForm(request.POST)
         if data.is_valid():
@@ -39,18 +57,44 @@ def register(request, form=UserRegistrationForm()):
 
 
 def see_about(request):
+    """Method shows a page about Minstagram.
+
+    Args:
+        request: http request from a user.
+    Returns:
+        HttpResponse: http response that shows "userpage/index.html" with infromation
+        about us if the user is not logged in and "userpage/about.html" else.
+    """
     if request.user.is_anonymous():
         return render(request, "userpage/index.html")
     return render(request, "userpage/about.html")
 
 
 def see_post(request, id=None):
+    """Method allows users to see their specific post.
+
+    Args:
+        request: http request from a user.
+        id: id of a specific post that the user wants to see.
+    Returns:
+        HttpResponse: http response that shows "userpage/mysubpost.html" with a
+        chosen post.
+    """
     details_post = get_object_or_404(UserPageData, id=id)
     return render(request, "userpage/mysubpost.html", {"details_post": details_post})
 
 
 @login_required
 def create_post(request):
+    """Method allows users to create a new post.
+
+    Args:
+        request: http request from a user.
+    Returns:
+        HttpResponse: http response that shows "userpage/myposts.html" with
+        a new post if the form is valid and "userpage/create_post.html" else.
+    """
+
     user_page_data = UserPageData(user=request.user)
     form = MakePostForm(request.POST or None, request.FILES or None, instance=user_page_data)
     if form.is_valid():
@@ -68,6 +112,15 @@ def create_post(request):
 
 @login_required
 def edit_post(request, id=None):
+    """Method allows users to edit their post.
+
+    Args:
+        request: http request from a user.
+        id: id of a specific post that a user wants to edit.
+    Returns:
+        HttpResponse: http response that shows "userpage/myposts.html" with
+        edited post if the form is valid and "userpage/edit_post.html" else.
+    """
     edt_post = get_object_or_404(UserPageData, id=id)
     form = MakePostForm(request.POST or None, request.FILES or None, instance=edt_post)
     if form.is_valid():
@@ -89,6 +142,15 @@ def edit_post(request, id=None):
 
 @login_required
 def delete_post(request, id=None):
+    """Method allows users to delete their post.
+
+    Args:
+        request: http request from a user.
+        id: id of a specific post that a user wants to delete.
+    Returns:
+        HttpResponse: http response that shows "userpage/myposts.html" without
+        deleted post.
+    """
     post = get_object_or_404(UserPageData, id=id)
     post.delete()
     return redirect("minstagram:home")
@@ -96,6 +158,14 @@ def delete_post(request, id=None):
 
 @login_required
 def see_friends(request):
+    """Method allows users to see their following.
+
+    Args:
+        request: http request from a user.
+    Returns:
+        HttpResponse: http response that shows "userpage/search.html" that
+        contains following.
+    """
     subscriptions = request.user.userprofiledata.subscriptions.all()
     following = UserProfileData.objects.filter(user__in=subscriptions)
     return render(request, "userpage/friends.html", {"following": following})
@@ -103,6 +173,14 @@ def see_friends(request):
 
 @login_required
 def find_friends(request):
+    """Method allows users to look for new friends.
+
+    Args:
+        request: http request from a user.
+    Returns:
+        HttpResponse: http response that shows "userpage/search.html" that
+        contains all users.
+    """
     profile_data = UserProfileData.objects.filter(~Q(user=request.user))
     following = request.user.userprofiledata.subscriptions.all()
     context_data = {
@@ -114,6 +192,15 @@ def find_friends(request):
 
 @login_required
 def see_user(request, id=None):
+    """Method allows users to see someones page.
+
+    Args:
+        request: http request from a user.
+        id: id of a specific user that the previous one wants to see.
+    Returns:
+        HttpResponse: http response that shows "userpage/someonesposts.html" that
+        contains posts of a user with a choosen user id.
+    """
     user = get_object_or_404(User, id=id)
     query = UserPageData.objects.filter(user=user).order_by("-time_created")
     following = request.user.userprofiledata.subscriptions.all()
@@ -129,12 +216,31 @@ def see_user(request, id=None):
 
 @login_required
 def see_user_post(request, user_id=None, post_id=None):
+    """Method allows users to see someones specific post.
+
+    Args:
+        request: http request from a user.
+        id: id of a specific user that the previous one wants to see.
+        post_id: id of a specific post that the previous one wants to see.
+    Returns:
+        HttpResponse: http response that shows "userpage/someonessubpost.html" with a
+        chosen post.
+    """
     details_post = get_object_or_404(UserPageData.objects.filter(user_id=user_id), id=post_id)
     return render(request, "userpage/someonessubpost.html", {"details_post": details_post})
 
 
 @login_required
 def follow(request, id=None):
+    """Method allows users to unfollow other users.
+
+    Args:
+        request: http request from a user.
+        id: id of a specific user that the previous one wants to unfollow.
+    Returns:
+        HttpResponse: http response that shows to a user a page he did
+        a request from.
+    """
     user = User.objects.get(id=id)
     request.user.userprofiledata.subscriptions.add(user)
     return redirect(request.META["HTTP_REFERER"])
@@ -142,6 +248,15 @@ def follow(request, id=None):
 
 @login_required
 def unfollow(request, id=None):
+    """Method allows users to follow other users.
+
+    Args:
+        request: http request from a user.
+        id: id of a specific user that the previous one wants to follow.
+    Returns:
+        HttpResponse: http response that shows to a user a page he did
+        a request from.
+    """
     user = User.objects.get(id=id)
     request.user.userprofiledata.subscriptions.remove(user)
     return redirect(request.META["HTTP_REFERER"])
@@ -149,6 +264,14 @@ def unfollow(request, id=None):
 
 @login_required
 def edit_info(request):
+    """Method allows users to change their profile info.
+
+    Args:
+        request: http request from a user.
+    Returns:
+        HttpResponse: http response that shows "userpage/mypost.html" to the client with
+        updated profile info if the form is valid and "userpage/edit_info.html" else.
+    """
     user_profile_data = UserProfileData.objects.get(user=request.user)
     profile_data = UserProfileData.objects.filter(user=request.user)
     form = EditInfo(request.POST or None, request.FILES or None, instance=user_profile_data)
